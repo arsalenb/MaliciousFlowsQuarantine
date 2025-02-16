cf8e5a0dee8fd3ad28031eab20555ec79d667259
arsalenb <arselenebidani@yahoo.fr>
2025-02-16 22:55:05 +0100

    Added logger to module

--- net/floodlightcontroller/unipi/maliciousflowsquarantine/MaliciousFlowQuarantine.java
+++ net/floodlightcontroller/unipi/maliciousflowsquarantine/MaliciousFlowQuarantine.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFType;
 import org.projectfloodlight.openflow.util.HexString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IFloodlightProviderService;
@@ -19,74 +21,75 @@ import net.floodlightcontroller.core.module.IFloodlightService;
 import net.floodlightcontroller.packet.Ethernet;
 
 public class MaliciousFlowsQuarantine implements IOFMessageListener, IFloodlightModule {
-	
-	protected IFloodlightProviderService floodlightProvider; // Reference to the provider
+    
+    protected IFloodlightProviderService floodlightProvider; // Reference to the provider
 
-	// Fixed DPID of Quarantine Switch
-	private final static String quarantineSwitchDpid = "00:00:00:00:00:00:00:04";
-	
-	// Rule timeouts
-	private final static short IDLE_TIMEOUT = 60;
-	private final static short HARD_TIMEOUT = 120; 
+    // Logger for the class
+    private static final Logger log = LoggerFactory.getLogger(MaliciousFlowsQuarantine.class);
 
-	
-	@Override
-	public String getName() {
-		return MaliciousFlowsQuarantine.class.getSimpleName();
-	}
+    // Fixed DPID of Quarantine Switch
+    private final static String quarantineSwitchDpid = "00:00:00:00:00:00:00:04";
+    
+    // Rule timeouts
+    private final static short IDLE_TIMEOUT = 60;
+    private final static short HARD_TIMEOUT = 120; 
 
-	@Override
-	public boolean isCallbackOrderingPrereq(OFType type, String name) {
-		return false;
-	}
+    @Override
+    public String getName() {
+        return MaliciousFlowsQuarantine.class.getSimpleName();
+    }
 
-	@Override
-	public boolean isCallbackOrderingPostreq(OFType type, String name) {
-		return false;
-	}
+    @Override
+    public boolean isCallbackOrderingPrereq(OFType type, String name) {
+        return false;
+    }
 
-	@Override
-	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
-		return null;
-	}
+    @Override
+    public boolean isCallbackOrderingPostreq(OFType type, String name) {
+        return false;
+    }
 
-	@Override
-	public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
-		return null;
-	}
+    @Override
+    public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+        return null;
+    }
 
-	@Override
-	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
-		Collection<Class<? extends IFloodlightService>> l =
-				new ArrayList<Class<? extends IFloodlightService>>();
-				l.add(IFloodlightProviderService.class);
-				return l;
-	}
+    @Override
+    public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
+        return null;
+    }
 
-	@Override
-	public void init(FloodlightModuleContext context) throws FloodlightModuleException {
-		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+    @Override
+    public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
+        Collection<Class<? extends IFloodlightService>> l = new ArrayList<>();
+        l.add(IFloodlightProviderService.class);
+        return l;
+    }
 
-	}
+    @Override
+    public void init(FloodlightModuleContext context) throws FloodlightModuleException {
+        floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+        log.info("MaliciousFlowsQuarantine module initialized.");
+    }
 
-	@Override
-	public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {
-		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+    @Override
+    public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {
+        floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+        log.info("MaliciousFlowsQuarantine module started and listening for PACKET_IN messages.");
+    }
 
-	}
+    @Override
+    public net.floodlightcontroller.core.IListener.Command receive(IOFSwitch sw, OFMessage msg,
+            FloodlightContext cntx) {
+        Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
 
-	@Override
-	public net.floodlightcontroller.core.IListener.Command receive(IOFSwitch sw, OFMessage msg,
-			FloodlightContext cntx) {
-	    Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
-	    // Print the source MAC address
-	    Long sourceMACHash = Ethernet.toLong(eth.getSourceMACAddress().getBytes());
-	    System.out.printf("MAC Address: {%s} seen on switch: {%s}\n",
-		HexString.toHexString(sourceMACHash),
-	         	sw.getId());   
-	    // Let other modules process the packet
-	    return Command.CONTINUE;
+        // Get the source MAC address
+        String sourceMAC = eth.getSourceMACAddress().toString();
 
-	}
+        // Log the event
+        log.info("MAC Address: {} seen on switch: {}", sourceMAC, sw.getId());
 
+        // Let other modules process the packet
+        return Command.CONTINUE;
+    }
 }
