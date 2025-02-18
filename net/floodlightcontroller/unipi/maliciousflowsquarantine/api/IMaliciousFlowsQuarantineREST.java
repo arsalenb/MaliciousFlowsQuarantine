cf31f4e6826a5ecc829af0896943a2ea05c0d7df
arsalenb <arselenebidani@yahoo.fr>
2025-02-18 21:15:30 +0100

    Create module rest API

---
+++ net/floodlightcontroller/unipi/maliciousflowsquarantine/api/IMaliciousFlowsQuarantineREST.java
@@ -0,0 +1,13 @@
+package net.floodlightcontroller.unipi.maliciousflowsquarantine.api;
+
+import net.floodlightcontroller.core.module.IFloodlightService;
+
+public interface IMaliciousFlowsQuarantineREST extends IFloodlightService {
+    
+    public Integer retrieveTotalBufferedPackets(String id);
+
+    public String markFlow(String clientIP, String serverIP, int bufferSize);
+
+    public String unmarkFlow(String clientIP, String serverIP, String mode);
+
+}
---
+++ net/floodlightcontroller/unipi/maliciousflowsquarantine/api/MaliciousFlowsQuarantineWebRoutable.java
@@ -0,0 +1,37 @@
+package net.floodlightcontroller.unipi.maliciousflowsquarantine.api;
+
+import org.restlet.Context;
+import org.restlet.Restlet;
+import org.restlet.routing.Router;
+
+import net.floodlightcontroller.core.web.ControllerSummaryResource;
+import net.floodlightcontroller.core.web.ControllerSwitchesResource;
+import net.floodlightcontroller.core.web.LoadedModuleLoaderResource;
+import net.floodlightcontroller.restserver.RestletRoutable;
+
+public class MaliciousFlowsQuarantineWebRoutable implements RestletRoutable {
+
+    @Override
+    public Restlet getRestlet(Context context) {
+        Router router = new Router(context);
+
+
+        router.attach("/controller/summary/json", ControllerSummaryResource.class);
+        router.attach("/module/loaded/json", LoadedModuleLoaderResource.class);
+        router.attach("/controller/switches/json", ControllerSwitchesResource.class);
+
+        // MaliciousFlowsQuarantine resources
+        router.attach("/mark", MarkFlow.class);
+        router.attach("/unmark/{method}", UnmarkFlow.class);
+        router.attach("/buffer/{id}", BufferSize.class);
+
+        return router;
+    }
+
+    @Override
+    public String basePath() {
+
+        return "/quarantine";
+    }
+    
+}
